64d454d01002b4bff404b80e100da9c25975aecf
Daniel Agg <daniel.agg@outlook.com>
2019-07-10 20:14:03 +0200

    Loader, LogItem components

---
+++ src/components/layout/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="progress blues lighten-4">
+      <div className="indeterminate blue" />
+    </div>
+  );
+};
+
+export default Loader;
--- src/components/logs/LogItemList.jsx
+++ src/components/logs/LogItemList.jsx
@@ -1,35 +1,36 @@
 import React, { useState, useEffect } from "react";
+import Loader from "../layout/Loader";
+import LogItem from "./LogItem";
 
 const LogItemList = () => {
   const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getLogs();
+    setTimeout(function() {
+      getLogs();
+    }, 1000);
   }, []);
 
   const getLogs = async () => {
-    setLoading(true);
     const res = await fetch("/logs");
     const data = await res.json();
-
-    console.log(data);
 
     setLogs(data);
     setLoading(false);
   };
 
   return loading ? (
-    <h4>Loading...</h4>
+    <Loader />
   ) : (
-    <ul className="collection-with-header">
+    <ul className="collection with-header">
       <li className="collection-header">
         <h4 className="center">System Logs</h4>
       </li>
       {!loading && logs.length === 0 ? (
         <p className="center">No logs to show</p>
       ) : (
-        logs.map(log => <li>{log.message}</li>)
+        logs.map(log => <LogItem key={log.id} log={log} />)
       )}
     </ul>
   );
